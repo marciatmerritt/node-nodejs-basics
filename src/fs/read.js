b18e2afa8e3375750b51658b168e19a78a96bf40
Marcia Merritt <marciatmerritt@gmail.com>
2024-10-03 22:36:15 -0400

    implement read.js functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import { promises as fs } from 'fs';
+import path from 'node:path';
+import { getDirname } from '../utils/dirname.js';
+import { ERROR_NO_ENTITY } from '../utils/constants.js';
+
+// implement function that prints content of the `fileToRead.txt` into console
+// if there's no file `fileToRead.txt` `Error` with message `FS operation failed` must be thrown
+
 const read = async () => {
-    // Write your code here 
+  const __dirName = getDirname(import.meta.url);
+  const dirPath = path.join(__dirName, 'files');
+  const readFileName = 'fileToRead.txt';
+  const readFilePath = path.join(dirPath, readFileName);
+
+    try {
+        const fileContent = await fs.readFile(readFilePath, 'utf-8');
+        console.log(fileContent);
+    } catch (error) {
+        if (error.code === ERROR_NO_ENTITY) {
+            throw new Error(`FS operation failed: ${readFileName} does not exist`);
+          } else {
+            throw new Error('FS operation failed - Unexpected error: ', error);
+          } 
+    }
 };
 
 await read();
