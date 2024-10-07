f7300da601f3f6cc416c3d01497ff7683ebc0df5
Marcia Merritt <marciatmerritt@gmail.com>
2024-10-06 22:17:21 -0400

    Implement env.js and args.js functionality

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
+import process from 'node:process';
+
+// implement function that parses command line arguments (given in format `--propName value --prop2Name value2`, you don't need to validate it)
+// and prints them to the console in the format `propName is value, prop2Name is value2`
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const results = {};
+
+  args.forEach((arg, i) => {
+    if (arg.startsWith('--')) {
+      const key = arg.replace('--', '');
+      const value = args[i + 1];
+      results[key] = value;
+    }
+  });
+  // Format and print the result
+  for (const [key, value] of Object.entries(results)) {
+    console.log(`${key} is ${value}`);
+  }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+import process from 'node:process';
+
+// implement function that parses environment variables with prefix `RSS_`
+// and prints them to the console in the format `RSS_name1=value1; RSS_name2=value2`
 const parseEnv = () => {
-    // Write your code here 
+  const rssEnvVars = Object.entries(process.env)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`);
+
+  if (rssEnvVars.length > 0) {
+    console.log(rssEnvVars.join('; '));
+  }
 };
 
 parseEnv();
