8abfbe5f8845301ba57a8c817375a37477485e06
Marcia Merritt <marciatmerritt@gmail.com>
2024-10-09 15:23:54 -0400

    Implement zip functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { createGzip } from 'node:zlib';
+import path from 'node:path';
+import { getDirname } from '../utils/dirname.js';
+
+// implement function that compresses file `fileToCompress.txt` to `archive.gz` using `zlib` and Streams API
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = getDirname(import.meta.url);
+  const inputFile = path.join(__dirname, 'files', 'fileToCompress.txt');
+  const outputFile = path.join(__dirname, 'files/archive.gz');
+
+  const readStream = createReadStream(inputFile);
+  const writeStream = createWriteStream(outputFile);
+  const gzipStream = createGzip();
+
+  try {
+    await new Promise((resolve, reject) => {
+        pipeline(readStream, gzipStream, writeStream, (error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+    console.log('File successfully compressed!');
+  } catch (error) {
+    console.error('Compression failed: ', error);
+  }
 };
 
 await compress();
