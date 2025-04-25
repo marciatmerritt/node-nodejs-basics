ec55ac2424518f9bc44037db1e89a99656331c7a
Marcia Merritt <marciatmerritt@gmail.com>
2025-04-25 18:43:36 -0400

    refactor: convert cjsToEsm module to esm syntax

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,21 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.cjs';
+import aFile from './files/a.json' assert { type: 'json' };
+import bFile from './files/b.json' assert { type: 'json' };
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const random = Math.random();
-
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
-} else {
-    unknownObject = require('./files/b.json');
+    unknownObject = aFile;
+}
+else {
+    unknownObject = bFile;
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +38,7 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
-
