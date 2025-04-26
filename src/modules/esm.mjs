4578ffd22101fce5c1c65de810cd58029c25cb14
Marcia Merritt <marciatmerritt@gmail.com>
2025-04-25 23:27:48 -0400

    fix: correct esm conversion for cjsToEsm module after testing

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,14 +1,20 @@
 import path from 'path';
 import { release, version } from 'os';
 import { createServer as createServerHttp } from 'http';
-import './files/c.cjs';
-import aFile from './files/a.json' assert { type: 'json' };
-import bFile from './files/b.json' assert { type: 'json' };
 import { fileURLToPath } from 'url';
+import { readFile } from 'fs';
+import { createRequire } from 'module';
+import './files/c.cjs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const require = createRequire(import.meta.url);
+const aFile = require('./files/a.json');
+const bFile = require('./files/b.json');
+
 const random = Math.random();
+
 let unknownObject;
 
 if (random > 0.5) {
